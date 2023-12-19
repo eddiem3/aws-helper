6f6a4c504e01a69f3a426d6c9b338bed505cb349
Eddie Massey III <eem33@case.edu>
2023-12-19 12:07:07 -0500

    add comments

--- aws_helper.py
+++ aws_helper.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+"""A module-level docstring
+
+Notice the comment above the docstring specifying the encoding.
+Docstrings do appear in the bytecode, so you can access this through
+the ``__doc__`` attribute. This is also what you'll see if you call
+help() on a module or any other Python object.
+"""
+
+
 import logging
 import boto3
 from botocore.exceptions import ClientError
@@ -5,7 +15,18 @@ from botocore.exceptions import ClientError
 import argparse
 
 
+
+
+
+
 def push_model_to_s3(model_path, bucket_name='yolov8-sku-detector'):
+     """
+     Compress a model in aws .tar.gz format and upload to S3 bucket.
+
+     :param str: The path to the model
+     #TODO HANDLE ERRORS
+    """
+    
     s3 = boto3.client('s3')
     archive_path = compress_model(model_path)
     model_uri = s3.upload_file(
